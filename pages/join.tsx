491c2508407c337d4aa870978b195c8d0ac5c098
Eric Feng <ericfeng610@gmail.com>
2021-11-06 20:47:28 -0400

    Added content text for join us page

--- pages/join.tsx
+++ pages/join.tsx
@@ -17,33 +17,12 @@ const joinCarousel = "/join/join-carousel-photo-2.png";
 
 // import processHandoff from "nonprofits/nonprofits-process-handoff.png";
 
-const applicationFaq = [
-    {
-        question: "CAN I APPLY IF I DON’T GO TO UWATERLOO?",
-        ans: "We plan to have our project lead and the nonprofit meet on a weekly or biweekly basis, depending on the flexibility of both parties, to sync up and discuss progress.",
-    },
-    {
-        question: "CAN I BE A PART OF BLUEPRINT WHILE ON A WORK TERM?",
-        ans: "...",
-    },
-    {
-        question:
-            "I DON’T HAVE A LOT OF DEV OR DESIGN EXPERIENCE, SHOULD I STILL APPLY?",
-        ans: "...",
-    },
-    {
-        question: "CAN I BE A PART OF BLUEPRINT IF I’M NOT IN WATERLOO?",
-        ans: "UW Blueprint will be fully remote in Winter 2021 so we're open to students joining our team from anywhere in the world. That being said, our meetings will usually be scheduled to accommodate people based in Waterloo (EST).",
-    },
-];
-
 const processData = [
     {
         title: "LEARN ABOUT BLUEPRINT",
         descripton:
             "Check out our current projects and team on our Facebook, Instagram and Medium.",
     },
-
     {
         title: "SUBMIT YOUR APPLICATION",
         descripton:
@@ -58,6 +37,45 @@ const processData = [
         title: "FINAL DECISION",
         descripton:
             "We will get back to you by Apr 12 to let you know our final decision. If we feel you’d be a good fit for the team, we would love for you to join! Get back to us as soon as possible regarding whether you would like to be part of our team next term, so we can get started with onboarding.",
+    },
+];
+
+const applicationFaq = [
+    {
+        question: "CAN I APPLY IF I DON’T GO TO UWATERLOO?",
+        ans: "We plan to have our project lead and the nonprofit meet on a weekly or biweekly basis, depending on the flexibility of both parties, to sync up and discuss progress.",
+    },
+    {
+        question: "CAN I BE A PART OF BLUEPRINT WHILE ON A WORK TERM?",
+        ans: "Yes, we are happy to take students on both academic and work terms.",
+    },
+    {
+        question:
+            "I DON’T HAVE A LOT OF DEV OR DESIGN EXPERIENCE, SHOULD I STILL APPLY?",
+        ans: "Yes! Blueprint is a great way to gain more experience as you will be surrounded by a community of peers with varying expertise in different fields, who can support you with mentorship. We care more about your drive to learn, and your passion for social good.",
+    },
+    {
+        question: "CAN I BE A PART OF BLUEPRINT IF I’M NOT IN WATERLOO?",
+        ans: "UW Blueprint will be fully remote in Winter 2021 so we're open to students joining our team from anywhere in the world. That being said, our meetings will usually be scheduled to accommodate people based in Waterloo (EST).",
+    },
+];
+
+const aboutOurTeam = [
+    {
+        question: "HOW DO YOU CHOOSE PROJECTS?",
+        ans: "We have an application portal on our website for nonprofits, and in addition, we also reach out to nonprofits chosen by our members. We set up calls with nonprofits and figure out whether their proposed project is a good fit for our team. Read more on our nonprofit page",
+    },
+    {
+        question: "DO I GET TO CHOOSE WHAT PROJECT I WORK ON?",
+        ans: "Before the term begins, our designers and developers have a chance to get to know the upcoming projects. They then fill out information about their skill set and preferences in our selection form so that Project Leads and Project Managers will choose their team accordingly. We try to make sure that most people get their first choice, but no guarantees can be made.",
+    },
+    {
+        question: "HOW DOES MENTORSHIP WORK IN BLUEPRINT?",
+        ans: "In our project teams, we look to ensure there is a good distribution in development experience. If you’re just starting out, you’ll always have someone you can turn to for help! We also have design meetings where designers can ask each other and the VP Design for feedback.",
+    },
+    {
+        question: "WHAT'S THE TIME COMMITMENT? HOW MANY HOURS PER WEEK?",
+        ans: "We would like Blueprint members to contribute around 10 hours per week. This is to ensure that we can deliver high-quality products to the organizations we partner with. We understand that it’s a big time commitment, and we would like you to make sure that your course load and extracurriculars allow for this type of commitment.",
     },
 ];
 
@@ -362,7 +380,7 @@ export default function Nonprofits() {
                         <HStack>
                             <FaqHeader>ABOUT OUR TEAM</FaqHeader>
                             <FaqAccordion>
-                                <ControlledAccordions data={applicationFaq} />
+                                <ControlledAccordions data={aboutOurTeam} />
                             </FaqAccordion>
                         </HStack>
                     </SectionContent>
