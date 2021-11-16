ced3263cc3e84c8a6cb19a120ea6befa8c9e742c
Mei Yi Niu <myniu@uwaterloo.ca>
2021-11-16 15:42:02 -0500

    Separate landing page graphics, refactor styling
    Co-authored-by: cynding <cynding@users.noreply.github.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,38 +2,34 @@ import React from "react";
 import Head from "next/head";
 import Image from "next/image";
 import Button from "../components/button/Button";
-import ReactPlayer from 'react-player';
+import ReactPlayer from "react-player";
 import { VStack, HStack } from "../components/Layout";
 
 import styled from "styled-components";
 
 // import { Header, Main, Cards, Footer } from "@components/css";
 import { Logo, Name } from "../components/common/blueprint-logo/BlueprintLogo";
-import { ImpactLeftGear, ImpactRightGear, HowItWorksGraphic, HowItWorksBg1, HowItWorksBg2, DemoLeftGear, DemoRightGear } from "../components/common/landing-page/LandingPage";
+import {
+    ImpactLeftGear,
+    ImpactRightGear,
+} from "components/common/landing-page/ImpactGraphics";
+import {
+    HowItWorksGraphic,
+    HowItWorksBg1,
+    HowItWorksBg2,
+} from "components/common/landing-page/HowItWorksGraphics";
+import {
+    DemoLeftGear,
+    DemoRightGear,
+} from "../components/common/landing-page/DemoGraphics";
 const landingSplash = "/home/home-landing-graphic.svg";
 const landingWave = "/home/home-landing-wave.svg";
 
 /* Styling */
+
 const Container = styled.div`
-    width: 100vw;
-    // display: flex;
-    // flex-direction: column;
-`;
-
-const Section = styled.div`
-    // position: relative;
-    // margin: auto;
-    // box-sizing: border-box;
-    // overflow-x: hidden;
     width: 100%;
-`;
-
-const LandingSection = styled(Section)`
-    // height: 1200px;
-
-    // background-color: ${(props) => props.theme.colors.C00};
-
-    & > div.imageWrapper {
+    & > div.landingGraphicWrapper {
         position: absolute;
         top: 0;
         left: 0;
@@ -80,25 +76,29 @@ const LandingSection = styled(Section)`
         right: 0;
         z-index: -1;
     }
-
-    & > div.missionSection {
-        height: 270px;
-        position: relative;
-        display: flex;
-        justify-content: center;
-
-        & > div.imageWrapper {
-            position: absolute;
-            top: 0;
-            left: 0;
-            right: 0;
-            height: 100%;
-            z-index: -1;
-        }
-    }
 `;
 
-const LandingContent = styled.div`
+const SectionTitle = styled.h2`
+    color: ${(props) => props.theme.colors.B10};
+    text-align: center;
+`;
+
+const SectionLine = styled.hr`
+    width: 15%;
+    margin: auto;
+    border-color: ${(props) => props.theme.colors.B10};
+    border-style: solid;
+    margin-top: 15px;
+`;
+
+const VerticalLine = styled.hr`
+    height: 120px; // TODO: make not px
+    border-color: ${(props) => props.theme.colors.B10};
+    border-style: solid;
+    margin: 30px;
+`;
+
+const LandingContainer = styled.div`
     height: 1024px;
     display: flex;
     flex-direction: column;
@@ -141,30 +141,25 @@ const LandingText = styled.h4`
     margin-bottom: 20px;
 `;
 
-const Description = styled.h3`
+const MissionContainer = styled.div`
+    height: 270px;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    & > div.landingWaveWrapper {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        height: 100%;
+        z-index: -1;
+    }
+`;
+
+const MissionDescription = styled.h3`
     color: ${(props) => props.theme.colors.B10};
     text-align: center;
     width: 75%;
-`;
-
-const SectionTitle = styled.h2`
-    color: ${(props) => props.theme.colors.B10};
-    text-align: center;
-`;
-
-const SectionLine = styled.hr`
-    width: 15%;
-    margin: auto;
-    border-color: ${(props) => props.theme.colors.B10};
-    border-style: solid;
-    margin-top: 15px;
-`;
-
-const VerticalLine = styled.hr`
-    height: 120px; // TODO: make not px
-    border-color: ${(props) => props.theme.colors.B10};
-    border-style: solid;
-    margin: 30px;
 `;
 
 const ImpactContainer = styled.div`
@@ -199,7 +194,7 @@ const ImpactStatDescription = styled.h6`
     font-weight: 600;
 `;
 
-const HowItWorksContent = styled.div`
+const HowItWorksContainer = styled.div`
     width: 100%;
     display: flex;
     flex-direction: column;
@@ -208,40 +203,37 @@ const HowItWorksContent = styled.div`
     margin-top: 180px;
 `;
 
-const SubSectionTitle = styled.h3`
+const HowItWorksSubSectionTitle = styled.h3`
     color: ${(props) => props.theme.colors.B10};
     text-align: center;
 `;
 
-const Cynthia = styled.h6`
+const HowItWorksSubtitle = styled.h6`
     color: ${(props) => props.theme.colors.B10};
     text-align: center;
     margin-top: 25px;
 `;
 
-const CynthiaHStack = styled(HStack)`
-    width: 75%
+const HowItWorksHStack = styled(HStack)`
+    width: 75%;
 `;
 
-const CynthiaVStack = styled(VStack)`
-`;
-
-const CynthiaParagraph = styled.p`
+const HowItWorksParagraph = styled.p`
     color: #000000;
     text-align: center;
     margin-top: 4px;
 `;
 
-const CynthiaLearnMore = styled.p`
+const HowItWorksLearnMore = styled.p`
     color: ${(props) => props.theme.colors.B10};
     text-align: center;
     margin-top: 18px;
     font-weight: 800;
 `;
 
-const DemoContent = styled.div`
+const DemoContainer = styled.div`
     width: 100%;
-    display: flex;s
+    display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
@@ -261,142 +253,145 @@ const Home: React.FC = () => {
                 {/* <link rel="icon" href="/favicon.ico" /> */}
             </Head>
             <Container>
-                <LandingSection>
-                    <div className="imageWrapper">
+                <div className="landingGraphicWrapper">
+                    <Image
+                        src={landingSplash}
+                        layout="fill"
+                        objectFit="cover"
+                    />
+                </div>
+                <LandingContainer>
+                    <LandingTitle>
+                        {" "}
+                        <LandingLogo /> blueprint
+                    </LandingTitle>
+                    <LandingText>
+                        tech for social good. built by students.
+                    </LandingText>
+                    <HStack>
+                        <LandingButton type="secondaryLight" to="/join">
+                            See our work
+                        </LandingButton>
+                        <LandingButton type="secondaryLight" to="/join">
+                            Join our team
+                        </LandingButton>
+                    </HStack>
+                </LandingContainer>
+                <div className="impactLeftGearWrapper">
+                    <ImpactLeftGear />
+                </div>
+                <div className="impactRightGearWrapper">
+                    <ImpactRightGear />
+                </div>
+                <MissionContainer>
+                    <div className="landingWaveWrapper">
                         <Image
-                            src={landingSplash}
+                            src={landingWave}
                             layout="fill"
                             objectFit="cover"
                         />
                     </div>
-                    <LandingContent>
-                        <LandingTitle> <LandingLogo/> blueprint</LandingTitle>
-                        <LandingText>
-                            tech for social good. built by students.
-                        </LandingText>
-                        <HStack>
-                            <LandingButton type="secondaryLight" to="/join">
-                                    See our work
-                            </LandingButton>
-                            <LandingButton type="secondaryLight" to="/join">
-                                    Join our team
-                            </LandingButton>
-                        </HStack>
-                    </LandingContent>
-                    <div className="impactLeftGearWrapper">
-                        <ImpactLeftGear/>
+                    <MissionDescription>
+                        Blueprint strives to make technology accessible and
+                        useful for those who create communities and promote
+                        social good.
+                    </MissionDescription>
+                </MissionContainer>
+                <ImpactContainer>
+                    <SectionTitle>
+                        Here's how we measure our impact
+                    </SectionTitle>
+                    <SectionLine />
+                    <ImpactStatContainer>
+                        <ImpactStat>
+                            <ImpactStatSmallNum>22</ImpactStatSmallNum>
+                            <ImpactStatDescription>
+                                NON-PROFIT PROJECTS
+                            </ImpactStatDescription>
+                        </ImpactStat>
+                        <ImpactStat>
+                            <ImpactStatBigNum>70,000</ImpactStatBigNum>
+                            <ImpactStatDescription>
+                                ACCUMULATED VOLUNTEER HOURS
+                            </ImpactStatDescription>
+                        </ImpactStat>
+                        <ImpactStat>
+                            <ImpactStatSmallNum>468</ImpactStatSmallNum>
+                            <ImpactStatDescription>
+                                STUDENT VOLUNTEERS
+                            </ImpactStatDescription>
+                        </ImpactStat>
+                    </ImpactStatContainer>
+                </ImpactContainer>
+                <div className="howItWorksBg1Wrapper">
+                    <HowItWorksBg1 />
+                </div>
+                <div className="howItWorksBg2Wrapper">
+                    <HowItWorksBg2 />
+                </div>
+                <HowItWorksContainer>
+                    <SectionTitle>How It Works</SectionTitle>
+                    <SectionLine />
+                    <div
+                        style={{
+                            marginTop: "30px",
+                        }}
+                    >
+                        <HowItWorksGraphic />
                     </div>
-                    <div className="impactRightGearWrapper">
-                        <ImpactRightGear/>
-                    </div>
-                    <div className="missionSection">
-                        <div className="imageWrapper">
-                            <Image
-                                src={landingWave}
-                                layout="fill"
-                                objectFit="cover"
-                            />
-                        </div>
-                        <Description>
-                            Blueprint strives to make technology accessible and
-                            useful for those who create communities and promote
-                            social good.
-                        </Description>
-                    </div>
-                    <ImpactContainer>
-                        <SectionTitle>Here's how we measure our impact</SectionTitle>
-                        <SectionLine/>
-                        <ImpactStatContainer>
-                            <ImpactStat>
-                                <ImpactStatSmallNum>
-                                    22
-                                </ImpactStatSmallNum>
-                                <ImpactStatDescription>
-                                    NON-PROFIT PROJECTS
-                                </ImpactStatDescription>
-                            </ImpactStat>
-                            <ImpactStat>
-                                <ImpactStatBigNum>
-                                    70,000
-                                </ImpactStatBigNum>
-                                <ImpactStatDescription>
-                                    ACCUMULATED VOLUNTEER HOURS
-                                </ImpactStatDescription>
-                            </ImpactStat>
-                            <ImpactStat>
-                                <ImpactStatSmallNum>
-                                    468
-                                </ImpactStatSmallNum>
-                                <ImpactStatDescription>
-                                    STUDENT VOLUNTEERS
-                                </ImpactStatDescription>
-                            </ImpactStat>
-                        </ImpactStatContainer>
-                    </ImpactContainer>
-                    <div className="howItWorksBg1Wrapper">
-                        <HowItWorksBg1/>
-                    </div>
-                    <div className="howItWorksBg2Wrapper">
-                        <HowItWorksBg2/>
-                    </div>
-                    <HowItWorksContent>
-                        <SectionTitle>How It Works</SectionTitle>
-                        <SectionLine/>
-                        <div
-                            style={{
-                                marginTop: "30px",
-                            }}
-                        >
-                        <HowItWorksGraphic/>
-                        </div>
-                        <CynthiaHStack>
-                            <CynthiaVStack>
-                                <SubSectionTitle>Student Volunteers</SubSectionTitle>
-                                <SectionLine/>
-                                <Cynthia>HAVE A PASSION FOR SOCIAL GOOD?</Cynthia>
-                                <CynthiaParagraph>
-                                Volunteer and grow your skills at UW Blueprint! Join a talented and vibrant community while creating technological solutions that make a real world impact. Student applications  open at the end of each term.
-                                </CynthiaParagraph>
-                                <CynthiaLearnMore>Learn More {">"}</CynthiaLearnMore>
-                            </CynthiaVStack>
-                            <VerticalLine/>
-                            <CynthiaVStack>
-                                <SubSectionTitle>Non Profit Partners</SubSectionTitle>
-                                <SectionLine/>
-                                <Cynthia>LET'S BUILD SOMETHING GREAT</Cynthia>
-                                <CynthiaParagraph>
-                                Whether you have a project idea or you don’t know where to start, our team of experienced Product Mangers, Designers, and Developers will be there every step of the way. Contact us to chat!
-                                </CynthiaParagraph>
-                                <CynthiaLearnMore>Learn More {">"}</CynthiaLearnMore>
-                            </CynthiaVStack>
-                        </CynthiaHStack>
-                    </HowItWorksContent>
-                    <div className="demoLeftGearWrapper">
-                        <DemoLeftGear/>
-                    </div>
-                    <div className="demoRightGearWrapper">
-                        <DemoRightGear/>
-                    </div>
-                    <DemoContent>
-                        <ReactPlayer url="https://www.youtube.com/watch?v=Dunh20k7gYA"/>
-                    </DemoContent>
-                </LandingSection>
-                <Section></Section>
+                    <HowItWorksHStack>
+                        <VStack>
+                            <HowItWorksSubSectionTitle>
+                                Student Volunteers
+                            </HowItWorksSubSectionTitle>
+                            <SectionLine />
+                            <HowItWorksSubtitle>
+                                HAVE A PASSION FOR SOCIAL GOOD?
+                            </HowItWorksSubtitle>
+                            <HowItWorksParagraph>
+                                Volunteer and grow your skills at UW Blueprint!
+                                Join a talented and vibrant community while
+                                creating technological solutions that make a
+                                real world impact. Student applications open at
+                                the end of each term.
+                            </HowItWorksParagraph>
+                            <HowItWorksLearnMore>
+                                Learn More {">"}
+                            </HowItWorksLearnMore>
+                        </VStack>
+                        <VerticalLine />
+                        <VStack>
+                            <HowItWorksSubSectionTitle>
+                                Non Profit Partners
+                            </HowItWorksSubSectionTitle>
+                            <SectionLine />
+                            <HowItWorksSubtitle>
+                                LET'S BUILD SOMETHING GREAT
+                            </HowItWorksSubtitle>
+                            <HowItWorksParagraph>
+                                Whether you have a project idea or you don’t
+                                know where to start, our team of experienced
+                                Product Mangers, Designers, and Developers will
+                                be there every step of the way. Contact us to
+                                chat!
+                            </HowItWorksParagraph>
+                            <HowItWorksLearnMore>
+                                Learn More {">"}
+                            </HowItWorksLearnMore>
+                        </VStack>
+                    </HowItWorksHStack>
+                </HowItWorksContainer>
+                <div className="demoLeftGearWrapper">
+                    <DemoLeftGear />
+                </div>
+                <div className="demoRightGearWrapper">
+                    <DemoRightGear />
+                </div>
+                <DemoContainer>
+                    <ReactPlayer url="https://www.youtube.com/watch?v=Dunh20k7gYA" />
+                </DemoContainer>
             </Container>
         </div>
-
-        // <div
-        //     style={{
-        //         display: "flex",
-        //         flexDirection: "column",
-        //         minHeight: "100vh",
-        //     }}
-        // >
-        //     <Header />
-        //     <Main />
-        //     <Cards />
-        //     <Footer />
-        // </div>
     );
 };
 
