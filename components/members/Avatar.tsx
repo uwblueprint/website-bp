7f55f30317724d09cf64ef8bc85f618c62d3469c
Tony Zhao <tawnyzhao@gmail.com>
2021-12-28 13:34:35 -0800

    Fix alignment on avatars

--- components/members/Avatar.tsx
+++ components/members/Avatar.tsx
@@ -3,13 +3,15 @@ import { FC } from "react";
 
 export const Avatar: FC<Member> = ({ name, position, profile }) => {
   return (
-    <figure className="flex flex-col items-center">
-      <img
-        className="rounded-full w-32"
-        src={profile || "/members/default.jpg"}
-      />
-      <p className="font-bold">{name}</p>
-      <p className="text-charcoal-500">{position}</p>
+    <figure className="flex flex-col items-start">
+      <div className="flex flex-col items-center">
+        <img
+          className="rounded-full w-32"
+          src={profile || "/members/default.jpg"}
+        />
+        <p className="font-bold">{name}</p>
+        <p className="text-charcoal-500">{position}</p>
+      </div>
     </figure>
   );
 };
