292054820776064d828d95d24cf45f46630cffaf
Tony Zhao <tawnyzhao@gmail.com>
2022-01-17 00:50:12 -0500

    Update roles map

--- components/members/utils.ts
+++ components/members/utils.ts
@@ -1,23 +1,29 @@
 export function roleType(role: string): number {
   const roles = new Map([
+    // EXEC TEAM ROLES
     ["president", 0],
     ["co-president", 0],
     ["vp engineering", 1],
     ["vp design", 2],
     ["vp product", 3],
-    ["vp internal", 4],
-    ["vp scoping", 5],
-    ["vp content", 6],
-    ["vp outreach", 7],
-    ["vp marketing", 8],
-    ["vp finance", 9],
-    ["technical lead", 10],
-    ["product manager", 11],
-    ["project mentor", 12],
-    ["designer", 13],
-    ["design mentor", 14],
-    ["developer", 15],
-    ["developer mentor", 16],
+    ["vp scoping", 4],
+    ["vp internal", 5],
+    ["vp external", 6],
+    ["vp communications", 7],
+    ["vp finance", 8],
+    // CLUB SUPPORT ROLES
+    ["graphic designer", 9],
+    ["content strategist", 10],
+    ["user researcher", 11],
+    ["internal operations lead", 12],
+    // PROJECT TEAM ROLES
+    ["technical lead", 13],
+    ["product manager", 14],
+    ["project mentor", 15],
+    ["designer", 16],
+    ["design mentor", 17],
+    ["developer", 18],
+    ["developer mentor", 19],
   ]);
 
   // If member has multiple roles, order by first one
