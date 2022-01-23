9f0e8895ad170b59eb37e1fad3ff0c2096a716bf
jaydenhs <j3hsiao@uwaterloo.ca>
2022-01-23 18:37:12 -0500

    Implement section

--- components/nonprofits/ContactUs.tsx
+++ components/nonprofits/ContactUs.tsx
@@ -4,19 +4,19 @@ import Button from "@components/common/Button";
 
 const ContactUs: FC = () => {
   return (
-    <section className="content flex items-center space-x-4">
-      <div className="flex-1">
-        <h4 className="text-blue uppercase">
-          Need help with your non profit and do not know where to start?
+    <section className="content flex items-center space-x-4 mb-24">
+      <div className="flex-1 w-full">
+        <h4 className="text-blue uppercase mb-7 md:mb-0">
+          Need help with your nonprofit and do not know where to start?
         </h4>
-        <hr className="w-96 mt-8 mb-12 text-blue" />
+        <hr className="w-96 mt-4 mb-10 text-blue hidden md:block" />
         <Link href="/contact">
           <a>
             <Button>Contact Us</Button>
           </a>
         </Link>
       </div>
-      <div className="flex-1">
+      <div className="flex-1 hidden md:block">
         <img
           src="/nonprofits/person-with-question.svg"
           alt="Person with question"
