6eabc1c499f01dbc4a9529fbc36e128a7ecc8169
jaydenhs <j3hsiao@uwaterloo.ca>
2022-01-25 21:37:47 -0500

    Implement navbar for mobile and tablet

--- components/common/NavBar.tsx
+++ components/common/NavBar.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import Link from "next/link";
 import Button from "@components/common/Button";
 
@@ -11,52 +11,91 @@ const ROUTES = [
 
 /** Navbar */
 const Navbar: FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className="w-full fixed z-20">
-      <div className="bg-blue">
-        <nav className="content flex flex-wrap justify-between pb-5 p-6 md:pb-6 box-border">
-          <div className="flex flex-col justify-center">
-            <Link href="/">
-              <a>
-                <img src="/common/logo-with-text.svg" alt="UW Blueprint logo" />
-              </a>
-            </Link>
-          </div>
-          <button className="lg:hidden">
-            <img className="" src="/common/menu.svg" alt="Menu" />
-          </button>
-          <div className="hidden lg:flex flex-wrap space-x-7">
-            <div className="flex items-center space-x-6">
-              {ROUTES.map((route) => (
-                <Link key={route.name} href={route.link}>
-                  <a className="text-sm font-poppins font-normal text-white">
-                    {route.name}
-                  </a>
-                </Link>
-              ))}
+    <>
+      <div className="w-full fixed z-30">
+        <div className="bg-blue">
+          <nav className="content flex flex-wrap space-x-4 lg:space-x-0 lg:justify-between pb-3 p-6 md:pb-6 box-border">
+            <button className="lg:hidden" onClick={() => setMenuOpen(true)}>
+              <img className="" src="/common/menu.svg" alt="Menu" />
+            </button>
+            <Logo />
+            <div className="hidden lg:flex flex-wrap space-x-7">
+              <div className="flex items-center space-x-6 text-sm">
+                <Links />
+              </div>
+              <div className="flex space-x-4">
+                <Buttons />
+              </div>
             </div>
-            <div className="flex space-x-4">
-              <Button
-                className="bg-transparent"
-                size="md"
-                variant="secondary"
-                invert
-                href="/join"
-              >
-                Join Our Team!
-              </Button>
-              <Button size="md" invert href="/nonprofits">
-                For Nonprofits
-              </Button>
-            </div>
-          </div>
-        </nav>
+          </nav>
+        </div>
+        <img
+          className="w-full -mt-0.5 md:hidden"
+          src="common/navbar-wave-sm.svg"
+        />
       </div>
-      <img
-        className="w-full -mt-0.5 md:hidden"
-        src="common/navbar-wave-sm.svg"
-      />
+      {menuOpen && (
+        <div className="fixed h-screen w-screen z-40 flex lg:hidden">
+          <div className="h-full bg-blue-200 w-64 pl-8 pr-12 py-14 space-y-12">
+            <Logo />
+            <div className="flex flex-col space-y-6 text-lg">
+              <Links />
+            </div>
+            <div className="flex flex-col space-y-5">
+              <Buttons extraClasses={"text-lg h-14 whitespace-nowrap"} />
+            </div>
+          </div>
+          <div
+            className="w-full bg-black bg-opacity-40"
+            onClick={() => setMenuOpen(false)}
+          />
+        </div>
+      )}
+    </>
+  );
+};
+
+const Logo = () => {
+  return (
+    <div className="flex flex-col justify-center">
+      <Link href="/">
+        <img src="/common/logo-with-text.svg" alt="UW Blueprint logo" />
+      </Link>
     </div>
+  );
+};
+
+const Links = () => {
+  return (
+    <>
+      {ROUTES.map((route) => (
+        <Link key={route.name} href={route.link}>
+          <a className="font-poppins font-normal text-white">{route.name}</a>
+        </Link>
+      ))}
+    </>
+  );
+};
+
+const Buttons: FC<{ extraClasses?: string }> = ({ extraClasses }) => {
+  return (
+    <>
+      <Button
+        className={`bg-transparent ${extraClasses}`}
+        size="md"
+        variant="secondary"
+        invert
+        href="/join"
+      >
+        Join Our Team!
+      </Button>
+      <Button className={`${extraClasses}`} size="md" invert href="/nonprofits">
+        For Nonprofits
+      </Button>
+    </>
   );
 };
 
