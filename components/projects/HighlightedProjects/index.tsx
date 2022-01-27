dff51bcc3f44ed7643d74d31f9b1e742d15eeb98
jaydenhs <j3hsiao@uwaterloo.ca>
2022-01-26 22:46:06 -0500

    Decrease padding

--- components/projects/HighlightedProjects/index.tsx
+++ components/projects/HighlightedProjects/index.tsx
@@ -7,7 +7,7 @@ const HighlightedProjects: FC = () => {
   return (
     <section className="content flex flex-col md:items-center py-24 pr-0 md:pr-4">
       <h2 className="text-blue mb-6 md:mb-0">Highlighted Projects</h2>
-      <hr className="hidden md:block w-20 mt-4 mb-24 text-blue" />
+      <hr className="hidden md:block w-20 mt-4 md:mb-16 lg:mb-24 text-blue" />
       <div className="flex overflow-x-auto snap-mandatory snap-x snap-always space-x-4 md:space-x-0 last:pr-4 md:last:pr-0 pb-4 pt-2 md:grid md:grid-cols-2 md:grid-rows-2 md:gap-10 ">
         {PROJECTS.map((project) => (
           <ProjectCard key={project.name} project={project} />
