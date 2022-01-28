ae042ba9cf06476d8462e3b96e557b00e2ba18d9
Jayden Hsiao <55633069+jaydenhs@users.noreply.github.com>
2022-01-28 09:35:33 -0500

    [Mobile + tablet] Implement Other Projects section (#45)
    * Implement Project card layout
    
    * Implement project modal + drawer
    
    * Decrease padding
    
    * Remove ß lol
    
    * Implement for mobile and tablet

--- components/projects/OtherProjects/index.tsx
+++ components/projects/OtherProjects/index.tsx
@@ -5,10 +5,10 @@ import PROJECTS from "@constants/other-projects.json";
 /** Other projects section of Projects page */
 const OtherProjects: FC = () => {
   return (
-    <section className="content flex flex-col items-center pt-24 pb-32">
+    <section className="content flex flex-col md:items-center pt-16 pb-40">
       <h2 className="text-blue">Other Projects</h2>
-      <hr className="w-20 mt-4 mb-24 text-blue" />
-      <div className="max-w-full flex space-x-10 -m-4 p-4 overflow-x-auto">
+      <hr className="hidden md:block w-20 mt-4 mb-8 md:mb-16 lg:mb-24 text-blue" />
+      <div className="max-w-full flex space-x-4 md:space-x-10 py-4 overflow-x-auto snap-mandatory snap-x snap-always">
         {PROJECTS.map((project) => (
           <ProjectCard key={project.name} project={project} />
         ))}
