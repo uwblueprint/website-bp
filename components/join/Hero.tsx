3f12be1b8e6db4e92bf8b4a4fdd0a194a5ca1581
jaydenhs <j3hsiao@uwaterloo.ca>
2022-01-31 20:52:46 -0500

    Conditionally render Apply Now button in join/hero

--- components/join/Hero.tsx
+++ components/join/Hero.tsx
@@ -1,8 +1,17 @@
 import { FC } from "react";
 import Button from "@components/common/Button";
+import {
+  APPLICATION_OPEN_DATETIME,
+  APPLICATION_CLOSE_DATETIME,
+} from "@constants/applications";
 
 /** Hero section */
 const Hero: FC = () => {
+  const currentDate = new Date();
+  const openDate = new Date(APPLICATION_OPEN_DATETIME);
+  const closeDate = new Date(APPLICATION_CLOSE_DATETIME);
+  const applicationIsLive = openDate < currentDate && currentDate < closeDate;
+
   return (
     <header className="mt-36 md:mt-0 md:h-[660px] w-full flex justify-around items-center bg-no-repeat bg-bottom bg-cover md:bg-join-hero pb-8 md:pb-0">
       <div className="max-w-6xl flex flex-col md:flex-row px-4">
@@ -15,16 +24,17 @@ const Hero: FC = () => {
               We are a group of friendly folks at the University of Waterloo
               dedicated to building technology for social good.
             </p>
-            {/* TODO: Global apply conditional? */}
-            {/* <Button
-              className="bg-transparent border-blue text-blue-100 md:border-white md:text-white"
-              // invert={true}
-              size="lg"
-              variant="secondary"
-              href="/nonprofits"
-            >
-              Apply now
-            </Button> */}
+            {applicationIsLive && (
+              <Button
+                className="bg-transparent border-blue text-blue-100 md:border-white md:text-white"
+                // invert={true}
+                size="lg"
+                variant="secondary"
+                href="/nonprofits"
+              >
+                Apply now
+              </Button>
+            )}
           </div>
         </div>
         <div>
