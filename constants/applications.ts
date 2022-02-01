e69f45dd2c7e7199c7f949862f5f89141514735a
Ahmed Hamodi <ahmed.r.hamodi@gmail.com>
2022-02-01 00:10:45 -0500

    Update student application dates (#52)

--- constants/applications.ts
+++ constants/applications.ts
@@ -2,25 +2,25 @@
  * Application open datetime
  * Format: MMM DD YYYY HH:MM:SS TZ
  */
-export const APPLICATION_OPEN_DATETIME = "Mar 6 2021 00:00:00 EST";
+export const APPLICATION_OPEN_DATETIME = "Feb 27 2022 00:00:00 EST";
 
 /**
  * Application close datetime
  * Format: MMM DD YYYY HH:MM:SS TZ
  */
-export const APPLICATION_CLOSE_DATETIME = "Mar 19 2021 23:59:59 EST";
+export const APPLICATION_CLOSE_DATETIME = "Mar 11 2022 23:59:59 EST";
 
 /**
  * Date that invites are sent out for interviews
  * Format: MMM DD
  */
-export const INVITE_DATE = "Mar 23";
+export const INVITE_DATE = "Mar 15";
 
 /**
  * Final decision date
  * Format: MMM DD
  */
-export const FINAL_DECISION_DATE = "Apr 12";
+export const FINAL_DECISION_DATE = "Apr 1";
 
 // URL of application page
 export const APPLICATION_LINK = "https://uwblueprint.org/apply";
