578cb082091fb987b8761130935f57850fee4da2
Lambert Liu <lambert0928@outlook.com>
2022-10-02 13:45:39 -0700

    Fix warnings in Projects page

--- components/projects/OtherProjects/index.tsx
+++ components/projects/OtherProjects/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, Fragment } from "react";
 import ProjectCard from "@components/projects/OtherProjects/ProjectCard";
 import PROJECTS from "@constants/other-projects.json";
 
@@ -10,7 +10,9 @@ const OtherProjects: FC = () => {
       <hr className="hidden md:block w-20 mt-4 mb-8 md:mb-16 lg:mb-24 text-blue" />
       <div className="max-w-full flex space-x-4 md:space-x-10 py-4 overflow-x-auto snap-mandatory snap-x snap-always">
         {PROJECTS.map((project) => (
-          <ProjectCard key={project.name} project={project} />
+          <Fragment key={project.name}>
+            <ProjectCard project={project} />
+          </Fragment>
         ))}
       </div>
     </section>
