776088fa3c35cf2170511e82f3abb2b83b8fda5e
unknown <evanmatt.wong@gmail.com>
2022-10-02 14:34:41 -0400

    built out skeleton and rough incomplete state management of section

--- components/apply/AppForm.tsx
+++ components/apply/AppForm.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import SelfIdentificationForm from "./SelfIdentification";
 import ShortAnswers from "./ShortAnswers";
 
 const AppForm: FC = () => {
@@ -6,6 +7,7 @@ const AppForm: FC = () => {
     <section className="container max-w-4xl px-4 mx-auto my-36 md:my-40">
       <h2 className="text-blue-100 mb-8">Student Application</h2>
       <ShortAnswers />
+      <SelfIdentificationForm />
     </section>
   );
 };
