69c801de805fbc9c905f7a1c8285c01e8a1a2e71
Lambert Liu <lambert0928@outlook.com>
2022-10-02 09:45:22 -0700

    Update AppForm container styling

--- components/apply/AppForm.tsx
+++ components/apply/AppForm.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 const AppForm: FC = () => {
   return (
-    <section className="container px-4 mx-auto my-36">
+    <section className="container max-w-6xl px-4 mx-auto my-36 md:my-40">
       <h2 className="text-blue-100 mb-4 md:mb-0">Student Application</h2>
     </section>
   );
