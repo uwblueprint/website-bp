47b36e6c9fc66ba0d42127a20830e4ae37af0d8d
Lambert Liu <lambert0928@outlook.com>
2022-10-02 14:42:33 -0700

    Create SelectInput, TextAreaInput, and TextInput

---
+++ components/common/TextAreaInput.tsx
@@ -0,0 +1,53 @@
+import { FC, useState } from "react";
+
+type Props = {
+  readonly id: string;
+  readonly labelText?: string;
+  readonly placeholder?: string;
+  readonly required?: boolean;
+  readonly onChange?: (value: string) => void;
+};
+
+const TextInput: FC<Props> = ({
+  id,
+  labelText,
+  placeholder,
+  required,
+  onChange,
+}) => {
+  const [inputValue, setInputValue] = useState<string | null>(null);
+
+  return (
+    <div className="flex flex-col space-y-2">
+      {labelText && (
+        <label htmlFor={id}>
+          {labelText}
+          {required && <span className="text-red-500">*</span>}
+        </label>
+      )}
+      <textarea
+        id={id}
+        className={
+          "text-charcoal-600 border border-charcoal-300 rounded-md px-4 py-3 border-l-4" +
+          (required && inputValue == ""
+            ? " border-l-pink-500"
+            : " border-l-charcoal-300")
+        }
+        style={{ resize: "none" }}
+        placeholder={placeholder}
+        required={required}
+        value={inputValue || ""}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+          onChange && onChange(e.target.value || "");
+        }}
+        rows={4}
+      />
+      {required && inputValue == "" && (
+        <span className="text-pink-500 text-sm">* Required</span>
+      )}
+    </div>
+  );
+};
+
+export default TextInput;
