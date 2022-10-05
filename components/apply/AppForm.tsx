ebb71079da2fcbd61141df4fdbb67a5c4ee96319
Lambert Liu <lambert0928@outlook.com>
2022-10-04 17:57:02 -0700

    Update TODO

--- components/apply/AppForm.tsx
+++ components/apply/AppForm.tsx
@@ -22,7 +22,7 @@ const AppForm: FC = () => {
   return (
     <section className="container max-w-4xl px-4 mx-auto my-36 md:my-40">
       <h2 className="text-blue-100 mb-8">Student Application</h2>
-      {/* TODO: Test Common Components - Remove before submitting */}
+      {/* TODO: Test common components - Remove before deploying */}
       <div className="grid grid-cols-2 gap-8">
         <TextInput
           id="test"
