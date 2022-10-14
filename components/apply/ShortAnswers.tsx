55492927d11d4ec1baaece235a49809c7d2bf9bc
Brian Tu <brianptu2@gmail.com>
2022-10-13 21:53:23 -0400

    Create short answers questions section (#83)
    * Create short answers section
    
    * Implement requested changes

---
+++ components/apply/ShortAnswers.tsx
@@ -0,0 +1,38 @@
+import { FC } from "react";
+import TextAreaInput from "@components/common/TextAreaInput";
+
+const AppForm: FC = () => {
+  return (
+    <div className="grid grid-cols-1 gap-3">
+      <h4 className="text-blue-100">Short Answers</h4>
+      <p className="text-charcoal-500 mb-4">
+        Please answer the following questions in a few sentences. (max length:
+        1000 characters, recommended max: 150 words)
+      </p>
+      <div className="grid grid-cols-1 gap-6">
+        <TextAreaInput
+          id="timezoneSA"
+          labelText="What timezone will you be based out of next term?"
+          required
+        />
+        <TextAreaInput
+          id="causeSA"
+          labelText="Tell us about a cause that resonates with you."
+          required
+        />
+        <TextAreaInput
+          id="communitySA"
+          labelText="Tell us about a community you're proud to be a part of and how you contributed to it."
+          required
+        />
+        <TextAreaInput
+          id="skillSA"
+          labelText="Tell us about a time you learned a new skill. What was your motivation to learn it and what was your approach?"
+          required
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AppForm;
