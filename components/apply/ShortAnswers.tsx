d5b36c223b5e8541ef2b97b546ac27c7b15193c2
Lambert Liu <lambert0928@outlook.com>
2022-10-14 18:40:15 -0400

    Remove redundant styling

--- components/apply/ShortAnswers.tsx
+++ components/apply/ShortAnswers.tsx
@@ -3,13 +3,13 @@ import TextAreaInput from "@components/common/TextAreaInput";
 
 const AppForm: FC = () => {
   return (
-    <div className="grid grid-cols-1 gap-3">
+    <div className="grid gap-3">
       <h4 className="text-blue-100">Short Answers</h4>
       <p className="text-charcoal-500 mb-4">
         Please answer the following questions in a few sentences. (max length:
         1000 characters, recommended max: 150 words)
       </p>
-      <div className="grid grid-cols-1 gap-6">
+      <div className="grid gap-6">
         <TextAreaInput
           id="timezoneSA"
           labelText="What timezone will you be based out of next term?"
