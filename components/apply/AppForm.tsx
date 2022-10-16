7134ef95e83469aad7836dc81386d4d1845be76d
Abubakar Bello <a4bello@uwaterloo.ca>
2022-10-16 14:45:00 -0400

    Application Position Preference Section (#84)
    * Implement Position Preference Section
    
    * Remove Lint Warnings
    
    * Fix Lint Error
    
    * Fix Extra Lint Errors
    
    * Implement PR Comments
    
    * Fix Build Error
    
    * Change Styling
    
    * Implement PR Styling Changes

--- components/apply/AppForm.tsx
+++ components/apply/AppForm.tsx
@@ -1,12 +1,22 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import BasicInfo from "./BasicInfo";
+import PositionPreference from "./PositionPreference";
 import ShortAnswers from "./ShortAnswers";
 
 const AppForm: FC = () => {
+  const [firstChoice, setFirstChoice] = useState("");
+  const [secondChoice, setSecondChoice] = useState("");
+
   return (
     <section className="container max-w-4xl px-4 mx-auto my-36 md:my-40">
       <h2 className="text-blue-100 mb-8">Student Application</h2>
       <BasicInfo />
+      <PositionPreference
+        firstChoice={firstChoice}
+        secondChoice={secondChoice}
+        onFirstChoiceChange={setFirstChoice}
+        onSecondChoiceChange={setSecondChoice}
+      />
       <ShortAnswers />
     </section>
   );
---
+++ components/apply/PositionPreference.tsx
@@ -0,0 +1,126 @@
+import { FC, useState, useEffect } from "react";
+import SelectInput from "@components/common/SelectInput";
+
+type Props = {
+  firstChoice: string;
+  secondChoice: string;
+  onFirstChoiceChange: (value: string) => void;
+  onSecondChoiceChange: (value: string) => void;
+};
+
+type MemberRoles = {
+  readonly label: string;
+  readonly value: string;
+};
+
+const MEMBER_ROLES: MemberRoles[] = [
+  {
+    label: "Project Developer",
+    value: "1",
+  },
+  {
+    label: "Product Designer",
+    value: "2",
+  },
+  {
+    label: "Product Manager",
+    value: "3",
+  },
+  {
+    label: "Project Lead",
+    value: "4",
+  },
+  {
+    label: "User Experience Researcher",
+    value: "5",
+  },
+  {
+    label: "Content Strategist",
+    value: "6",
+  },
+  {
+    label: "Graphic Designer",
+    value: "7",
+  },
+  {
+    label: "VP Engineering",
+    value: "8",
+  },
+  {
+    label: "VP Product",
+    value: "9",
+  },
+  {
+    label: "VP Talent",
+    value: "10",
+  },
+  {
+    label: "VP Communications",
+    value: "11",
+  },
+  {
+    label: "VP Finance & Operations",
+    value: "12",
+  },
+  {
+    label: "VP Project Scoping",
+    value: "13",
+  },
+  {
+    label: "VP External",
+    value: "14",
+  },
+  {
+    label: "VP Internal",
+    value: "15",
+  },
+];
+
+const PositionPreference: FC<Props> = ({
+  firstChoice,
+  secondChoice,
+  onFirstChoiceChange,
+  onSecondChoiceChange,
+}: Props) => {
+  const [secondChoiceRoles, setSecondChoiceRoles] = useState(
+    MEMBER_ROLES.filter((roles) => roles.value !== firstChoice),
+  );
+
+  useEffect(() => {
+    const SECOND_CHOICE_MEMBER_ROLES = MEMBER_ROLES.filter(
+      (roles) => roles.value !== firstChoice,
+    );
+    setSecondChoiceRoles(SECOND_CHOICE_MEMBER_ROLES);
+    if (firstChoice == secondChoice && firstChoice != "") {
+      const newSecondChoice = SECOND_CHOICE_MEMBER_ROLES?.[0]?.value || "";
+      onSecondChoiceChange(newSecondChoice);
+    }
+  }, [firstChoice]);
+
+  return (
+    <div className="grid gap-3 my-8">
+      <h4 className="text-blue-100">Position Preference</h4>
+      <p className="text-charcoal-500 mb-4">
+        Please select the position(s) you are interested in.*
+      </p>
+      <div className="grid gap-6">
+        <SelectInput
+          id="firstChoicePP"
+          labelText="What is your first choice role?"
+          options={MEMBER_ROLES}
+          required
+          onChange={onFirstChoiceChange}
+        />
+        <SelectInput
+          id="secondChoicePP"
+          labelText="Optional: What is your second choice role? (only select if you want to be considered for this role)"
+          options={secondChoiceRoles}
+          required={false}
+          onChange={onSecondChoiceChange}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PositionPreference;
