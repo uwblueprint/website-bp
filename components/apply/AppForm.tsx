c72d5eae388815da596c6b4eb5e3bd0b3064bb78
Linna <38363056+linnall@users.noreply.github.com>
2022-10-16 10:57:24 -0700

    Basic Information Section (#86)
    * basic layout
    
    * adds margins for resume and term type sections
    
    * removes state management and modifies styling
    
    * Make form component styling consistent
    
    Co-authored-by: Lambert Liu <lambert0928@outlook.com>

--- components/apply/AppForm.tsx
+++ components/apply/AppForm.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
+import BasicInfo from "./BasicInfo";
 import ShortAnswers from "./ShortAnswers";
 
 const AppForm: FC = () => {
   return (
     <section className="container max-w-4xl px-4 mx-auto my-36 md:my-40">
       <h2 className="text-blue-100 mb-8">Student Application</h2>
+      <BasicInfo />
       <ShortAnswers />
     </section>
   );
