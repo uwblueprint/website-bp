156aa2d16b042ec85bf4a238e834805a6395e3a5
unknown <evanmatt.wong@gmail.com>
2022-11-07 09:32:48 -0500

    implemented hard coded student table

---
+++ components/common/Table.tsx
@@ -0,0 +1,63 @@
+import { FC } from "react";
+
+interface Student {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  resumeLink: string;
+  detailsLink: string;
+}
+
+type PageProps = {
+  readonly: Student[];
+};
+
+const Table: FC<PageProps> = ({ students }) => {
+  return (
+    <div className="max-w-2xl mx-auto">
+      <div className="flex flex-col space-y-2">
+        <table className="table-fixed text-left border-collapse">
+          <thead className="pt-4">
+            <tr className="border-b-[2px]">
+              <th className="py-2">#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Resume</th>
+              <th>Details</th>
+            </tr>
+          </thead>
+          <tbody>
+            {students.map((app, i) => (
+              <tr className="border-b-[1px] border-indigo-500">
+                <td className="pr-4 py-4 w-4">{`${i + 1}`}</td>
+                <td className="pr-4 w-32">{`${
+                  app.firstName + " " + app.lastName
+                }`}</td>
+                <td className="pr-4 w-56">{`${app.email}`}</td>
+                <td className="pr-4 w-20">
+                  <a
+                    href={`${app.resumeLink}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Resume Link
+                  </a>
+                </td>
+                <td className="pr-4 w-16">
+                  <a
+                    href={`https://uwblueprint.org/admin/student-details/${app.detailsLink}`}
+                  >
+                    Details
+                  </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Table;
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,10 +1,44 @@
 import { NextPage } from "next";
 import ProtectedRoute from "@components/context/ProtectedRoute";
+import Table from "@components/common/Table";
+
+interface Student {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  resumeLink: string;
+  detailsLink: string;
+}
+
+const TABLE_MOCK_DATA: Student[] = [
+  // left this here for easier testing
+  // we would replace this with data fetching functionality
+  {
+    id: 1,
+    firstName: "Evan",
+    lastName: "Wong",
+    email: "evanwong@uwblueprint.org",
+    resumeLink: "asdlf.com",
+    detailsLink: "asdf",
+  },
+  {
+    id: 2,
+    firstName: "Evan",
+    lastName: "Wongd",
+    email: "evanwondg@uwblueprint.org",
+    resumeLink: "asdlf.com",
+    detailsLink: "asdf",
+  },
+];
 
 const Admin: NextPage = () => {
   return (
     <ProtectedRoute>
-      <>Admin Page</>
+      <>
+        Admin Page
+        <Table students={TABLE_MOCK_DATA} />
+      </>
     </ProtectedRoute>
   );
 };
