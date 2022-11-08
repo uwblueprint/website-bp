3f76b2e6935310a1af1beb85fb584e46cadfafa9
Abubakar Bello <a4bello@uwaterloo.ca>
2022-11-07 23:36:53 -0500

    Admin Page Implementation (#98)
    * Admin Page Implementation
    
    * Create logo-with-text-blue.svg
    
    * Fix PR Comments
    
    Co-authored-by: Lambert Liu <lambert0928@outlook.com>

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,10 +1,38 @@
+import React from "react";
 import { NextPage } from "next";
 import ProtectedRoute from "@components/context/ProtectedRoute";
+import roleSpecificJson from "@constants/role-specific-questions.json";
+
+const memberRoles = roleSpecificJson.map(({ role }) => role);
 
 const Admin: NextPage = () => {
   return (
     <ProtectedRoute>
-      <>Admin Page</>
+      <div className="container max-w-4xl px-4 mx-auto my-8">
+        <div className="flex justify-between items-center my-16">
+          <img src="/common/logo-with-text-blue.svg" alt="UW Blueprint Logo" />
+          <button className="text-blue-100">Logout</button>
+        </div>
+        <h2 className="text-blue-100 mb-8">Students</h2>
+        <div className="flex justify-between">
+          <select
+            id="roles"
+            name="roles"
+            className="border-l-charcoal-300 text-charcoal-600 border border-charcoal-300 rounded-md px-4 py-3 border-l-4 focus:outline-none focus:ring-1 focus:ring-blue-100 focus:border-blue-100"
+            style={{ minHeight: "25px" }}
+          >
+            <option value="" disabled>
+              Select an option
+            </option>
+            {memberRoles.map((value) => (
+              <option key={value} value={value}>
+                {value}
+              </option>
+            ))}
+          </select>
+          <button className="text-blue-100">Export CSV</button>
+        </div>
+      </div>
     </ProtectedRoute>
   );
 };
