9545188dc48a84c7e3ec5c450ebf14dd77e0125c
Linna <38363056+linnall@users.noreply.github.com>
2022-11-27 17:47:41 -0500

    Adds filtering applications by role to admin page (#102)
    * merge main
    
    * adds logic for filtering apps by role
    
    * change default value of roleSelected
    
    * remove console.log

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -18,6 +18,7 @@ import { firebaseDb } from "@utils/firebase";
 const memberRoles = roleSpecificJson.map(({ role }) => role);
 
 const Admin: NextPage = () => {
+  const [roleSelected, setRoleSelected] = useState("default");
   const [students, setStudents] = useState<Student[]>([]);
 
   useEffect(() => {
@@ -34,6 +35,8 @@ const Admin: NextPage = () => {
                 lastName: allApps[id].lastName,
                 email: allApps[id].email,
                 resumeLink: allApps[id].resumeUrl,
+                firstChoiceRole: allApps[id].firstChoiceRole,
+                secondChoiceRole: allApps[id].secondChoiceRole,
               });
             }
           });
@@ -61,10 +64,9 @@ const Admin: NextPage = () => {
             name="roles"
             className="border-l-charcoal-300 text-charcoal-600 border border-charcoal-300 rounded-md px-4 py-3 border-l-4 focus:outline-none focus:ring-1 focus:ring-blue-100 focus:border-blue-100"
             style={{ minHeight: "25px" }}
+            onChange={(e) => setRoleSelected(e.target.value)}
           >
-            <option value="" disabled>
-              Select an option
-            </option>
+            <option value="default">Select an option</option>
             {memberRoles.map((value) => (
               <option key={value} value={value}>
                 {value}
@@ -74,7 +76,7 @@ const Admin: NextPage = () => {
           <button className="text-blue-100">Export CSV</button>
         </div>
         <div className="my-8">
-          <ApplicationsTable students={students} />
+          <ApplicationsTable students={students} roleSelected={roleSelected} />
         </div>
       </div>
     </ProtectedRoute>
