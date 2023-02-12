32b58b4b74510be0296721c81526b262db25d70f
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2023-02-12 14:46:57 -0500

    Update application timeline for the current term

--- constants/applications.ts
+++ constants/applications.ts
@@ -3,32 +3,32 @@
  * Format: MMM DD YYYY HH:MM:SS TZ
  * Note: be mindful of day light saving time changes!
  */
-export const APPLICATION_OPEN_DATETIME = "Oct 25 2022 00:00:00 EDT";
+export const APPLICATION_OPEN_DATETIME = "Feb 19 2023 00:00:00 EDT";
 
 /**
  * Application close datetime
  * Format: MMM DD YYYY HH:MM:SS TZ
  * Note: be mindful of day light saving time changes!
  */
-export const APPLICATION_CLOSE_DATETIME = "Nov 5 2022 23:59:59 EDT";
+export const APPLICATION_CLOSE_DATETIME = "Mar 3 2023 23:59:59 EDT";
 
 /**
  * Date that invites are sent out for interviews
  * Format: MMM DD
  */
-export const INVITE_DATE = "Nov 13";
+export const INVITE_DATE = "Mar 19";
 
 /**
  * Final decision date
  * Format: MMM DD
  */
-export const FINAL_DECISION_DATE = "Dec 5";
+export const FINAL_DECISION_DATE = "Apr 10";
 
 // Term this application is for
-export const APPLICATION_TERM = "Winter 2023";
+export const APPLICATION_TERM = "Spring 2023";
 
 // URL of application page
-export const APPLICATION_LINK = "https://www.uwblueprint.org/apply";
+export const APPLICATION_LINK = "/apply";
 
 // Calculate if the application is live
 const _currentDate = new Date();
