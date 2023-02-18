a2c4e72cf7882b133672087326770d95aba9660c
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2023-02-18 04:09:21 -0500

    Allow non-required selects to have their values cleared

--- components/common/SelectInput.tsx
+++ components/common/SelectInput.tsx
@@ -37,7 +37,7 @@ const SelectInput: FC<Props> = ({
           style={{ minHeight: "50px" }}
           required={required}
         >
-          <option value="" disabled>
+          <option value="" disabled={required}>
             Select an option
           </option>
           {options.map((value) => (
