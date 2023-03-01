30627a7731db84296a1eb94789f936636554fc58
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2023-02-28 22:02:56 -0500

    Let grad students apply

--- components/apply/BasicInfo.tsx
+++ components/apply/BasicInfo.tsx
@@ -78,10 +78,10 @@ const BasicInfo: FC<Props> = ({ values, readOnly }: Props) => {
         />
         <TextInput
           id="academicYear"
-          labelText="Academic Year"
+          labelText={`Academic Year (or "Graduate student")`}
           placeholder="e.g. 2A, 4B"
           value={values.academicYear}
-          regexMatch={/^[1-5][A-B]$/}
+          regexMatch={/^(1-5][A-B])|[Gg]raduate student$/}
           errorMessage="Please enter a valid academic year (e.g. 2A, 4B)"
           readOnly={readOnly}
           required
