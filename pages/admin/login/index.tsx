f49e15b4ee03b2a468a5d0b3551bd66f96e8b5c3
Kevin <kc.chen.kevin@gmail.com>
2023-03-21 21:15:21 -0400

    applied figma design

--- pages/admin/login/index.tsx
+++ pages/admin/login/index.tsx
@@ -41,14 +41,21 @@ const Login: NextPage = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <form className="flex flex-col" onSubmit={(e) => {login(e)}}>
-        <label htmlFor="username">Username: </label>
-        <input type="text" name="username" required onChange={(e) => {setLoginState({...loginState, username: e.target.value})}} />
-        <label htmlFor="password">Password: </label>
-        <input type="text" name="password" required onChange={(e) => {setLoginState({...loginState, password: e.target.value})}} />
-        <button className="mt-2 rounded-md border-solid border-2 border-black" type="submit">Log In</button>
-      </form>
+    <div className="flex justify-center items-center h-screen bg-sky-100">
+      <div className="flex flex-col justify-start items-start bg-white rounded-[8px] w-[500px] h-[500px] shadow-[0px_4px_20px_rgba(0,0,0,0.25)]">
+        <img
+          src="/common/logo-with-text-blue.svg"
+          alt="UW Blueprint Logo"
+          style={{ position: "relative", top: 37, left: 26 }}
+        />
+        <form className="flex flex-col space-y-[26px] pl-[88px] mt-[90px]" onSubmit={(e) => {login(e)}}>
+          <label className="text-blue-100 font-poppins font-[600] text-[20px]" htmlFor="username">Username</label>
+          <input className="h-[36px] w-[300px] border-1 border-[#aaaaaa] rounded-[4px]" type="text" name="username" required onChange={(e) => {setLoginState({...loginState, username: e.target.value})}} />
+          <label className="text-blue-100 font-poppins font-[600] text-[20px]" htmlFor="password">Password</label>
+          <input className="h-[36px] w-[300px] border-1 border-[#aaaaaa] rounded-[4px]" type="text" name="password" required onChange={(e) => {setLoginState({...loginState, password: e.target.value})}} />
+        </form>
+        <button className="justify-self-start mt-[26px] rounded-full border-solid border-2 border-blue-100 bg-blue-100 text-white ml-[88px] px-[32px] py-[13px]" type="submit">Sign In</button>
+      </div>
     </div>
   )
 }
