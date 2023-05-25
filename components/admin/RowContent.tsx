c450e6ef0327f7b20c00eb0a75ec4488c18dd85c
Chris Abey <chris.abey587@gmail.com>
2023-05-24 21:10:25 -0400

    pushed changes

---
+++ components/admin/RowContent.tsx
@@ -0,0 +1,90 @@
+import React, { FC, useState, useEffect } from "react";
+
+const queries = {
+  applicationsByRole: `
+            query applicationsByRole($firstChoice: String!) {
+              applicationsByRole(firstChoice: $firstChoice) {
+                id
+                firstName
+                lastName
+                academicYear
+                resumeUrl
+                program
+                status
+              }
+            }
+          `,
+  dashboardsByApplicationId: `
+          query dashboardsByApplicationId($applicationId: Int!) {
+            dashboardsByApplicationId(applicationId: $applicationId) {
+                reviewerEmail
+                passionFSG
+                teamPlayer
+                desireToLearn
+                skillCategory
+            }
+          }
+        `,
+};
+
+const RowContent: FC = () => {
+  const [applications, setApplications] = useState<any[]>([]);
+  const [dashboards, setDashboards] = useState<any[]>([]);
+  useEffect(() => {
+    applications.map((app) => {
+      dashboardsByApplicationId();
+    });
+  }, [applications]);
+
+  useEffect(() => {
+    applicationsByRole();
+  }, []);
+
+  const applicationsByRole = () => {
+    fetch("http://localhost:5000/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: queries.applicationsByRole,
+        variables: {
+          firstChoice: "project developer",
+        },
+      }),
+    }).then(
+      async (res) =>
+        await res
+          .json()
+          .then((result) => setApplications(result.data.applicationsByRole)),
+    );
+  };
+  const dashboardsByApplicationId = () => {
+    fetch("http://localhost:5000/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: queries.dashboardsByApplicationId,
+        variables: {
+          applicationId: 1,
+        },
+      }),
+    }).then(
+      async (res) =>
+        await res
+          .json()
+          .then((result) =>
+            setDashboards(result.data.dashboardsByApplicationId),
+          ),
+    );
+  };
+  return (
+    <div>
+      {applications.map((app) => app.status)}
+      {dashboards.map((dashboard) => dashboard.reviewerEmail)}
+    </div>
+  );
+};
+export default RowContent;
--- components/admin/Table.tsx
+++ components/admin/Table.tsx
@@ -1,12 +1,14 @@
 import React, { FC } from "react";
 import RowHeader from "./RowHeader";
 import TableTitle from "./TableTitle";
+import RowContent from "./RowContent";
 
 const Table: FC = () => {
   return (
     <div className="px-8">
       <TableTitle />
       <RowHeader />
+      <RowContent />
     </div>
   );
 };
