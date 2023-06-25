dc6431a52000dc63a1550309a0187ca4d3b78bfb
aaggarwal10 <anish.5@hotmail.com>
2023-06-25 14:15:39 -0700

    Revert "fix login bug by removing onauthChange callback (#148)"
    This reverts commit e527281e5d3cc1340a9146bea427ceff3e774da4.

--- components/context/ProtectedRoute.tsx
+++ components/context/ProtectedRoute.tsx
@@ -1,6 +1,8 @@
 import { ReactChild, ReactElement, useEffect, useState } from "react";
 import Loading from "@components/common/Loading";
 import { queries } from "graphql/queries";
+import getAccessToken from "common/authHelper";
+import { auth } from "@utils/firebase";
 import { useRouter } from "next/router";
 
 type Props = {
@@ -22,48 +24,54 @@ const ProtectedRoute = ({ children, allowedRoles }: Props): ReactElement => {
     isAuthorized: false,
   });
   useEffect(() => {
-    const accessToken = localStorage.getItem("accessToken");
-    if (accessToken == null) {
+    auth.onAuthStateChanged(async () => {
       setAuthStatus({
-        loading: false,
+        loading: true,
         isAuthorized: false,
       });
-      return;
-    }
+      const accessToken = await getAccessToken();
+      if (accessToken == null) {
+        setAuthStatus({
+          loading: false,
+          isAuthorized: false,
+        });
+        return;
+      }
 
-    fetch("http://localhost:5000/graphql", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        query: queries.isAuthorizedByRole,
-        variables: {
-          accessToken,
-          roles: allowedRoles,
+      fetch("http://localhost:5000/graphql", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-      }),
-    })
-      .then(
-        async (res) =>
-          await res.json().then((result) => {
-            if (result.data.isAuthorizedByRole) {
-              setAuthStatus({
-                loading: false,
-                isAuthorized: true,
-              });
-            } else {
-              setAuthStatus({
-                loading: false,
-                isAuthorized: false,
-              });
-            }
-          }),
-      )
-      .catch((e) => {
-        console.error("Auth Validation Error");
-        console.error(e);
-      });
+        body: JSON.stringify({
+          query: queries.isAuthorizedByRole,
+          variables: {
+            accessToken,
+            roles: allowedRoles,
+          },
+        }),
+      })
+        .then(
+          async (res) =>
+            await res.json().then((result) => {
+              if (result.data.isAuthorizedByRole) {
+                setAuthStatus({
+                  loading: false,
+                  isAuthorized: true,
+                });
+              } else {
+                setAuthStatus({
+                  loading: false,
+                  isAuthorized: false,
+                });
+              }
+            }),
+        )
+        .catch((e) => {
+          console.error("Auth Validation Error");
+          console.error(e);
+        });
+    });
   }, [allowedRoles]);
 
   if (!authStatus.loading && !authStatus.isAuthorized)
