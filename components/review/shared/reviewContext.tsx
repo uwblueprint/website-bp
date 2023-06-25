b4162e277fbfcfda38bedf02f7b0fa2e97ffccbd
aaggarwal10 <anish.5@hotmail.com>
2023-06-25 14:16:40 -0700

    Revert "Define basic framework for reviews (#144)"
    This reverts commit 634f7133f5105f0cafff64aab479e0e8392963f7.

--- components/review/shared/reviewContext.tsx
+++
@@ -1,6 +0,0 @@
-import { ReviewStage } from "pages/review";
-import { createContext } from "react";
-
-export const ReviewSetStageContext = createContext<
-  null | ((newValue: ReviewStage) => void)
->(null);
--- pages/review/submit-success.tsx
+++
@@ -1,7 +0,0 @@
-import { NextPage } from "next";
-
-const ReviewsSubmitSuccess: NextPage = () => {
-  return <p>Thank you! Your submission for John Doe has been submitted!</p>;
-};
-
-export default ReviewsSubmitSuccess;
