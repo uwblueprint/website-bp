ddd5efe648d2fc94350c971a9516587ad72d7069
Anish Aggarwal <anish.5@hotmail.com>
2023-06-28 18:08:37 -0700

    fix login bug by removing onauthChange callback (#148) (#150)
    * fix login bug by removing onauthChange callback
    
    * lint
    
    * remove unused imports
    
    ---------
    
    Co-authored-by: TW1234567 <117593511+TW1234567@users.noreply.github.com>
    Co-authored-by: Thomas Wu <orangeleaves@v1040-wn-rt-b-24-91.campus-dynamic.uwaterloo.ca>

--- components/common/Login.tsx
+++ components/common/Login.tsx
@@ -2,6 +2,7 @@ import { auth } from "@utils/firebase";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { ReactElement } from "react";
 import Button from "./Button";
+import { fetchGraphql } from "@utils/makegqlrequest";
 
 const Login = (): ReactElement => {
   const signInWithGoogle = async () => {
