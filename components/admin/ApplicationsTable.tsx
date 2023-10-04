f5e7f9e9515e967faa1db18b82ee3500dead2a7f
HeetShah <heetshah7642@gmail.com>
2023-10-03 20:28:10 -0400

    removes console logs

--- components/admin/ApplicationsTable.tsx
+++ components/admin/ApplicationsTable.tsx
@@ -54,18 +54,15 @@ const queries = {
               }
               }
             }
-          `
+          `,
 };
 
 const ApplicationsTable: React.FC = () => {
   const [applications, setApplications] = useState<any[]>([]);
-  useEffect(() => {
-    console.log(applications);
-  }, [applications])
 
   useEffect(() => {
     applicationsByRole();
-  }, [])
+  }, []);
 
   const getSkillCategory = (dashboards: any) => {
     if (dashboards?.length >= 2) {
@@ -84,7 +81,6 @@ const ApplicationsTable: React.FC = () => {
     }
     return dashboards.length == 1 ? dashboards[0].skillCategory : "";
   };
-
 
   const applicationsByRole = () => {
     fetchGraphql(queries.applicationsByRole, {
@@ -165,7 +161,6 @@ const ApplicationsTable: React.FC = () => {
     });
 
   const getTableRows = (): StudentRow[] => {
-    console.log(applications);
     const rows: StudentRow[] = applications.map((application) => {
       const app = application.application;
       const reviewers = application.reviewers;
