3c7efabad58bd034c6d541f1bdfcd8b621559890
HeetShah <heetshah7642@gmail.com>
2023-10-12 20:40:48 -0400

    adds back filters to dashboard

--- components/admin/ApplicationsTable.tsx
+++ components/admin/ApplicationsTable.tsx
@@ -136,11 +136,11 @@ const ApplicationsTable: React.FC = () => {
             fontFamily: "Source Sans Pro",
           },
         },
-        MUIDataTableToolbar: {
-          root: {
-            display: "none",
-          },
-        },
+        // MUIDataTableToolbar: {
+        //   root: {
+        //     display: "none",
+        //   },
+        // },
         MuiTableSortLabel: {
           root: {
             color: `${theme.colors.B10} !important`,
@@ -186,42 +186,23 @@ const ApplicationsTable: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col space-y-2">
-      <table className="table-fixed text-left border-collapse">
-        <thead className="pt-4">
-          <tr className="border-b-[2px]">
-            <th className="py-2">#</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Resume</th>
-            <th>Details</th>
-          </tr>
-        </thead>
-        <tbody>
-          {students.map((app: Student, i: number) => (
-            <tr className="border-b-[1px] border-charcoal-400" key={i}>
-              <td className="pr-4 py-4 w-4">{`${i + 1}`}</td>
-              <td className="pr-4 w-32">{`${
-                app.firstName + " " + app.lastName
-              }`}</td>
-              <td className="pr-4 w-56">{`${app.email}`}</td>
-              <td className="pr-4 w-20 text-blue-100">
-                <a
-                  href={`${app.resumeLink}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Resume
-                </a>
-              </td>
-              <td className="pr-4 w-16 text-blue-100">
-                <a href={`/admin/student-details/${app.id}`}>Details</a>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <MuiThemeProvider theme={getMuiTheme()}>
+      <MUIDataTable
+        options={{
+          elevation: 0,
+          responsive: "standard",
+          print: false,
+          download: false,
+          viewColumns: true,
+          filter: true,
+          search: true,
+          pagination: true,
+        }}
+        title={""}
+        data={getTableRows()}
+        columns={getTableColumns()}
+      />
+    </MuiThemeProvider>
   );
 };
 
