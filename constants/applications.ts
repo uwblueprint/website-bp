1cc2a3beea2f1a320c000408827903b6220913de
HeetShah <heetshah7642@gmail.com>
2023-11-12 00:27:11 -0700

    extends app deadline

--- constants/applications.ts
+++ constants/applications.ts
@@ -14,7 +14,7 @@ export const APPLICATION_OPEN_DATETIME = dayjs.tz(
  * Format: YYYY-MM-DD HH:MM:SS
  */
 export const APPLICATION_CLOSE_DATETIME = dayjs.tz(
-  "2023-11-11 23:59:59",
+  "2023-11-12 23:59:59",
   "America/Toronto",
 );
 
