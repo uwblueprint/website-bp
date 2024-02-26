cea63263da5e643eff8545e34af66e9f85e66b50
Lorena Rosati <lrosati@outlook.com>
2024-02-25 23:57:55 -0500

    Finished most of styling

--- components/review/stages/reviewEndSuccessStage.tsx
+++ components/review/stages/reviewEndSuccessStage.tsx
@@ -1,3 +1,22 @@
-export const ReviewEndSuccessStage: React.FC = () => {
-  return <h1>Thanks for reviewing!</h1>;
+import Button from "@components/common/Button";
+import Hero from "../../landing/Hero";
+
+interface Props {
+  name: string;
+}
+
+export const ReviewEndSuccessStage: React.FC<Props> = ({ name }) => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex flex-col items-center w-3/5 space-y-3">
+        <img src="../../../common/review-page-banner.svg" className="w-1/2 md:w-1/5"/>
+        <h2>Thank you!</h2>
+        <h4 className="text-center">
+          Your review for <span className="text-blue">{name}</span> has been submitted!
+        </h4>
+        <Button size="sm" href="">All Done!</Button>
+        <Button size="sm" variant="secondary" href="">Edit Review</Button>
+      </div>
+    </div>
+  );
 };
