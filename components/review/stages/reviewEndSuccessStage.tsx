78fee5661bcc1bf27bf684646821c4f60180e586
Lorena Rosati <lrosati@outlook.com>
2024-02-26 20:30:19 -0500

    Finished styling, added links to buttons

--- components/review/stages/reviewEndSuccessStage.tsx
+++ components/review/stages/reviewEndSuccessStage.tsx
@@ -1,21 +1,44 @@
 import Button from "@components/common/Button";
-import Hero from "../../landing/Hero";
+import { ReviewStage } from "pages/review";
+import { ReviewSetStageContext } from "../shared/reviewContext";
 
 interface Props {
   name: string;
 }
 
+//add href for buttons
+
 export const ReviewEndSuccessStage: React.FC<Props> = ({ name }) => {
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="flex flex-col items-center w-3/5 space-y-3">
-        <img src="../../../common/review-page-banner.svg" className="w-1/2 md:w-1/5"/>
-        <h2>Thank you!</h2>
-        <h4 className="text-center">
-          Your review for <span className="text-blue">{name}</span> has been submitted!
+      <div className="flex flex-col items-center w-3/5">
+        <div className="flex flex-col items-center">
+          <img
+            src="../../../common/review-page-banner.svg"
+            className="w-1/3 mb-3"
+          />
+          <h2 className="mb-7">Thank you!</h2>
+        </div>
+        <h4 className="text-center mb-7">
+          Your review for <span className="text-blue">{name}</span> has been
+          submitted!
         </h4>
-        <Button size="sm" href="">All Done!</Button>
-        <Button size="sm" variant="secondary" href="">Edit Review</Button>
+        <ReviewSetStageContext.Consumer>
+          {(setStage) => (
+            <div className="flex flex-col items-center">
+              <Button size="sm" href="/admin" className="mb-2">
+                All Done!
+              </Button>
+              <Button
+                size="sm"
+                variant="secondary"
+                onClick={() => setStage?.(ReviewStage.INFO)}
+              >
+                Edit Review
+              </Button>
+            </div>
+          )}
+        </ReviewSetStageContext.Consumer>
       </div>
     </div>
   );
