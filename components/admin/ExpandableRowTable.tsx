309303406a9bfbaa0a13070c1091219dc567e7ca
Kush Patel <kushpatel1928@gmail.com>
2024-03-18 20:01:40 -0400

    edit button working properly

--- components/admin/ExpandableRowTable.tsx
+++ components/admin/ExpandableRowTable.tsx
@@ -34,29 +34,33 @@ const ExpandableRowTable: React.FC<InnerTableProps> = ({ data, columns, edit}) =
   console.log("columns", columns);
   console.log("data", data);
 
-  // for (let i=0; i<columns.length; i++) {
-  //   if (columns[i].name == "PFSG"){
-  //     initData["PFSG"] = columns[i].value
-  //   } else if (columns[i].name == "Team Player") {
-  //     initData["team"] = columns[i].value
-  //   } else if (columns[i].name == "D2L") {
-  //     initData["D2L"] = columns[i].value
-  //   } else if (columns[i].name == "Skill"){
-  //     initData["skill"] = columns[i].value
-  //   }
-  // }
-
-  //console.log("data", data);
+  const [PFSG1, setPFSG1] = useState(initData1["PFSG"])
+  const [team1, setTeam1] = useState(initData1["team"])
+  const [D2L1, setD2L1] = useState(initData1["D2L"])
+  const [skill1, setSkill1] = useState(initData1["skill"])
 
   const [PFSG, setPFSG] = useState(initData2["PFSG"])
   const [team, setTeam] = useState(initData2["team"])
   const [D2L, setD2L] = useState(initData2["D2L"])
   const [skill, setSkill] = useState(initData2["skill"])
 
-  const handleChange = (event: any) => {
+  const handleChange = (event: any, row: number) => {
+    console.log("event", event);
+    console.log("row", row);
     const { id, value } = event.target;
 
-    if(edit) {
+    if(edit && row == 0) {
+      if (id == "PFSG"){
+        setPFSG1(value)
+      } else if (id == "Team Player") {
+        setTeam1(value)
+      } else if (id == "D2L") {
+        setD2L1(value)
+      } else if (id == "Skill"){
+        setSkill1(value)
+      }
+    }
+    if (edit && row == 1) {
       if (id == "PFSG"){
         setPFSG(value)
       } else if (id == "Team Player") {
@@ -81,8 +85,19 @@ const ExpandableRowTable: React.FC<InnerTableProps> = ({ data, columns, edit}) =
         },
       };
     }
-    const getValue = (value: number, column: MUIDataTableColumn) => {
-      if (edit) {
+    const getValue = (value: number, column: MUIDataTableColumn, row: number) => {
+      if (edit && row == 0) {
+        if (column.name == "PFSG"){
+          return PFSG1
+        } else if (column.name == "Team Player") {
+          return team1
+        } else if (column.name == "D2L") {
+          return D2L1
+        } else if (column.name == "Skill"){
+          return skill1
+        }
+      }
+      else if (edit && row == 1) {
         if (column.name == "PFSG"){
           return PFSG
         } else if (column.name == "Team Player") {
@@ -100,18 +115,17 @@ const ExpandableRowTable: React.FC<InnerTableProps> = ({ data, columns, edit}) =
 
     // Add custom body render for the values when edit is true
     if (edit) {
-      //if (column.name === "Reviewer Name") {
       if (column.name === "PFSG" || column.name === "Team Player" || column.name === "D2L" || column.name === "Skill") {
         return {
           ...column,
           options: {
             ...column.options,
-            customBodyRender: (value: number) => {
+            customBodyRender: (value: number, tableMeta: any) => {
               return (
                 <input 
                   type="number" 
-                  value={getValue(value, column)} 
-                  onChange={handleChange}
+                  value={getValue(value, column, tableMeta.rowIndex)} 
+                  onChange={(e) => handleChange(e, tableMeta.rowIndex)}
                   id={column.name}
                   style={{ 
                     width: '40px', 
