6abbef805ab23a63984b2e0033ef296189845189
Yaser Nossibeh <75911486+ya5er@users.noreply.github.com>
2024-11-06 20:58:50 -0500

    Modified application dates for W25 (#216)
    * Modified application dates
    
    * Fixed application close date

--- constants/applications.ts
+++ constants/applications.ts
@@ -5,7 +5,7 @@ import dayjs from "@utils/dayjs";
  * Format: YYYY-MM-DD HH:MM:SS
  */
 export const APPLICATION_OPEN_DATETIME = dayjs.tz(
-  "2024-07-08 00:00:00",
+  "2024-11-07 00:00:00",
   "America/Toronto",
 );
 
@@ -14,7 +14,7 @@ export const APPLICATION_OPEN_DATETIME = dayjs.tz(
  * Format: YYYY-MM-DD HH:MM:SS
  */
 export const APPLICATION_CLOSE_DATETIME = dayjs.tz(
-  "2024-07-22 23:59:59",
+  "2024-11-14 23:59:59",
   "America/Toronto",
 );
 
@@ -25,16 +25,16 @@ export const APPLICATION_CLOSE_DATETIME_WITH_GRACE_PERIOD =
  * Date that invites are sent out for interviews
  * Format: MMM DD
  */
-export const INVITE_DATE = "the end of the school term";
+export const INVITE_DATE = "the end of november";
 
 /**
  * Final decision date
  * Format: MMM DD
  */
-export const FINAL_DECISION_DATE = "August 2nd";
+export const FINAL_DECISION_DATE = "the middle of December";
 
 // Term Blueprint is currently recruiting for (1 term after the current term)
-export const APPLICATION_TERM = "Fall 2024";
+export const APPLICATION_TERM = "Winter 2025";
 
 // URL of application page
 export const APPLICATION_LINK = "/apply";
