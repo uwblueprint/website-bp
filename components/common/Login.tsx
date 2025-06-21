1da59fdd53347ca81eb9999973c9847c3bf696cd
eric martin <ern.martin12@gmail.com>
2025-06-21 12:07:13 -0400

    fixed login glitch (#230)

--- components/common/Login.tsx
+++ components/common/Login.tsx
@@ -24,7 +24,7 @@ const Login = (): ReactElement => {
               "refreshToken",
               result.data.loginWithGoogle.refreshToken,
             );
-            router.back();
+            router.push("/admin");
           }
         },
       );
