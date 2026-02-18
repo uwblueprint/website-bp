2a18d7a7c205201c4371c0a5e5000f21432b670e
David Liang <davidliang100@gmail.com>
2026-02-17 20:44:26 -0500

    complete rubric

--- components/review/stages/ReviewRubric.tsx
+++ components/review/stages/ReviewRubric.tsx
@@ -1,25 +1,33 @@
-import { ReviewStage } from "../shared/constants";
-import { ReviewScores } from "../shared/types";
+import { ReviewStage } from "pages/review";
 
-export interface Props {
+interface Props {
   scoringCriteria: string[];
-  scores: ReviewScores;
+  scores: Map<ReviewStage, number>;
   currentStage: ReviewStage;
 }
 
-export const ReviewRubric = ({ scoringCriteria }: Props) => {
+export const ReviewRubric: React.FC<Props> = ({ scoringCriteria }) => {
   return (
-    <div className="shrink gap-8">
-      <div className="inline-flex gap-6 flex-col items-start">
-        {scoringCriteria.map((criteria, idx) => {
-          return (
-            <div key={criteria} className="flex items-center shrink-0 self-stretch p-4 gap-6">
-              <h5 className="flex flex-col w-5 text-[28px]">{idx + 1}</h5>
-              <div className="flex-1 flex-col text-base">{criteria}</div>
-            </div>
-          );
-        })}
-      </div>
+    <div className="flex flex-col gap-6 w-full font-source">
+      {scoringCriteria.map((criteria, idx) => (
+        <div key={idx} className="flex flex-col gap-2">
+          <span
+            className="font-poppins text-[16px] font-medium text-blue-200"
+            style={{
+              lineHeight: "140%",
+              fontFeatureSettings: "'liga' off, 'clig' off",
+            }}
+          >
+            Level {idx + 1}
+          </span>
+          <p
+            className="text-[16px] font-normal text-charcoal-600"
+            style={{ lineHeight: "140%" }}
+          >
+            {criteria}
+          </p>
+        </div>
+      ))}
     </div>
   );
 };
